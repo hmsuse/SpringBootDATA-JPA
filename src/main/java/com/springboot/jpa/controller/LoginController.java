d2dccd16399fe4931cabee95c383b892a369bbf6
hmsuse <meet2hms@gmail.com>
2021-11-12 16:59:01 +0530

    cross origin

--- src/main/java/com/springboot/jpa/controller/LoginController.java
+++ src/main/java/com/springboot/jpa/controller/LoginController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,6 +20,7 @@ import com.springboot.jpa.UserAccountDetailsRepository;
 import com.springboot.jpa.UserAccountdetails;
 
 @RestController
+@CrossOrigin(origins="*")
 @RequestMapping("/api/v1")
 public class LoginController {
 
